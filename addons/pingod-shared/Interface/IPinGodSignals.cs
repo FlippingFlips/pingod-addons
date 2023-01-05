db229b292568a760a4f1b6a16773daedf0e7bb5e
Dave <horsepin@protonmail.com>
2023-01-05 22:21:41 +0000

    added some interfaces for the main pingod-game classes

---
+++ addons/pingod-shared/Interface/IPinGodSignals.cs
@@ -0,0 +1,9 @@
+﻿public interface IPinGodSignals
+{
+    event BallDrainedEventHandler BallDrained;
+    event BallEndedEventHandler BallEnded;
+    event BallSavedEventHandler BallSaved;
+    event BonusEndedEventHandler BonusEnded;
+    event MultiBallEndedEventHandler MultiBallEnded;
+    event ScoreEntryEndedEventHandler ScoreEntryEnded;
+}
