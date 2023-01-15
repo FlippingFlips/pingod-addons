68158f5f172d13ab6019a3ec3eb9f8eea632f016
Dave <horsepin@protonmail.com>
2023-01-15 22:28:16 +0000

    More logging methods for the P-ROC logger.

--- examples/pingod-netproc-pdb/proc/PinGodProcLogger.cs
+++ examples/pingod-netproc-pdb/proc/PinGodProcLogger.cs
@@ -1,13 +1,35 @@
-﻿using NetProc.Domain;
+﻿using Godot;
+using NetProc.Domain;
 using NetProc.Domain.PinProc;
 using PinGod.Core;
+using static System.Net.Mime.MediaTypeNames;
 
 internal class PinGodProcLogger : ILogger
 {
+    public LogLevel LogLevel { get; set; }
+
     public void Log(string text) => Logger.Info("P-ROC:", text);
 
     public void Log(string text, LogLevel logLevel = LogLevel.Info)
     {
-        Logger.Info("P-ROC:", logLevel, ":", text);
+        if (CanLog(logLevel))
+        {
+            GD.Print(text);
+        }
     }
+
+    public void Log(LogLevel logLevel = LogLevel.Info, params object[] logObjs)
+    {
+        if (CanLog(logLevel))
+        {
+            GD.Print(logObjs);
+        }
+    }
+
+    public void Log(params object[] logObjs)
+    {
+        GD.Print(logObjs);
+    }
+
+    bool CanLog(LogLevel logLevel) => logLevel <= LogLevel;
 }
