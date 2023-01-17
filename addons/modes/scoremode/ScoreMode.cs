ab53c3ac240285a3fdfc440bd329e2ad7781a5c7
Dave <horsepin@protonmail.com>
2023-01-17 17:38:10 +0000

    ScoreMode: Updated to be better for inheriting. This default mode uses PinGodPlayers but this can be overriden now.

--- addons/modes/scoremode/ScoreMode.cs
+++ addons/modes/scoremode/ScoreMode.cs
@@ -10,17 +10,20 @@ namespace PinGod.Modes
     /// </summary>
     public partial class ScoreMode : Node
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        protected IPinGodGame pinGod;
+
         [ExportCategory("debug")]
         [Export] bool _isDebugMode = false;
+        [Export] protected bool _show_main_score_multiplayer = true;
 
         /// <summary>
         /// Show Player ones ScoreP1 label if set to true. Normally in a pinball the scorep1 would not display with main score unless multi-player
         /// </summary>
         [ExportCategory("options")]
-        [Export] bool _single_player_p1_visible = false;
-
-        [Export] bool _show_main_score_multiplayer = true;
-
+        [Export] protected bool _single_player_p1_visible = false;
         #region Node paths to select in scene
         [ExportCategory("score labels")]
         [Export] NodePath _ballInfoLabel = null;
@@ -28,25 +31,20 @@ namespace PinGod.Modes
         [Export] NodePath _scoreLabel = null;
         [Export] Array<NodePath> _scoreLabels = null;
         #endregion
-
-        /// <summary>
-        /// 
-        /// </summary>
-        protected IPinGodGame pinGod;
-
         #region Labels from Scene
         /// <summary>
         /// 
         /// </summary>
         protected Label ballInfolabel;
         /// <summary>
-        /// Main score label
-        /// </summary>
-        protected Label scoreLabel;
-        /// <summary>
         /// Player information label
         /// </summary>
         protected Label playerInfoLabel;
+
+        /// <summary>
+        /// Main score label
+        /// </summary>
+        protected Label scoreLabel;
         /// <summary>
         /// Other player scoreLabels.
         /// </summary>
@@ -76,20 +74,11 @@ namespace PinGod.Modes
                     SetupDebugMode();
                 }
             }
-        }
-
-        private void SetupDebugMode()
-        {
-            Players = new PinGodPlayer[] { new(), new(), new(), new() };
-            Logger.Debug(nameof(ScoreMode), $": DEBUG. Fake players");
-            Players[0].Points = 280000;
-            Players[1].Points = 260000;
-            Players[2].Points = 240000;
-            Players[3].Points = 220000;
-        }
+        }        
 
         /// <summary>
-        /// Update current player and ball
+        /// Retrieves all labels in the scene. Checks to see if we have <see cref="_scoreLabels"/> set. <para/>
+        /// Will call <see cref="OnScoresUpdated"/>
         /// </summary>
         public override void _Ready()
         {
@@ -126,98 +115,111 @@ namespace PinGod.Modes
         }
 
         /// <summary>
-        /// Updates all the labels text
+        /// override this but this will invoke <see cref="OnScoresUpdated"/> to update scene labels
+        /// </summary>
+        public virtual void OnBallStarted() => OnScoresUpdated();
+
+        /// <summary>
+        /// Updates all the labels text. Override <see cref="UpdateMainScore"/>, <see cref="UpdatePlayerScores"/>, <see cref="UpdatePlayerBallInfo"/>
         /// </summary>
         public virtual void OnScoresUpdated()
         {
-            //Logger.LogDebug("scores updated");
-            if (pinGod?.Players?.Count > 0)
+            if (!_isDebugMode)
             {
-                if (scoreLabel != null)
-                {
-                    if (pinGod.Player.Points > -1)
-                    {
-                        //more than 1 player, show main score?
-                        if (pinGod.Players.Count > 1 && !_show_main_score_multiplayer)
-                        {
-                            scoreLabel.Text = null;
-                        }
-                        //set main score label
-                        else { scoreLabel.Text = pinGod.Player.Points.ToScoreString(); }
-                    }
-                    else
-                    {
-                        scoreLabel.Text = null;
-                    }
-                }
-
-                //in all player labels update their scores
-                int i = 0;
-                foreach (var player in pinGod.Players)
-                {
-                    //this hides displaying a multi-player score for the single player.
-                    if (pinGod.Players.Count == 1 && i == 0 && !_single_player_p1_visible)
-                    {
-                        i++;
-                        continue;
-                    }
-                    var lbl = ScoreLabels[i];
-                    if (lbl != null)
-                    {
-                        if (player.Points > -1)
-                        {
-                            lbl.Text = player.Points.ToScoreString();
-                        }
-                        else
-                        {
-                            lbl.Text = null;
-                        }
-                    }
-                    i++;
-                }
-
-                //update current player and ball
-                if (ballInfolabel != null)
-                {
-                    ballInfolabel.Text = Tr("BALL") + " " + pinGod.BallInPlay.ToString();
-                }
-                if (playerInfoLabel != null)
-                {
-                    playerInfoLabel.Text = $"{Tr("PLAYER")}: {pinGod.CurrentPlayerIndex + 1}";
-                    ballInfolabel.Text = Tr("BALL") + " " + (pinGod.BallInPlay).ToString();
-                }
-
+                UpdateMainScore();
+                UpdatePlayerScores();
+                UpdatePlayerBallInfo();
             }
             else
             {
-                if (_isDebugMode)
+                //main score
+                if (Players.Length > 1 && !_show_main_score_multiplayer) scoreLabel.Text = null;
+                else scoreLabel.Text = (Players[0].Points).ToScoreString();
+                //all players
+                for (int i = 0; i < ScoreLabels.Length - 1; i++) { ScoreLabels[i].Text = (Players[i].Points).ToScoreString(); }
+                //ball info
+                if (ballInfolabel != null) ballInfolabel.Text = Tr("BALL") + " 3";
+                //player info
+                if (playerInfoLabel != null) playerInfoLabel.Text = Tr("PLAYER") + " PLAYER: 1";
+            }
+        }
+
+        /// <summary>
+        /// in all player labels update their scores
+        /// </summary>
+        public virtual void UpdatePlayerScores()
+        {
+            if (pinGod?.Players?.Count <= 0) return;
+
+            int i = 0;
+            foreach (var player in pinGod.Players)
+            {
+                //this hides displaying a multi-player score for the single player.
+                if (pinGod.Players.Count == 1 && i == 0 && !_single_player_p1_visible)
                 {
-                    if (Players.Length > 1 && !_show_main_score_multiplayer)
+                    i++;
+                    continue;
+                }
+                var lbl = ScoreLabels[i];
+                if (lbl != null)
+                {
+                    if (player.Points > -1)
                     {
-                        scoreLabel.Text = null;
+                        lbl.Text = player.Points.ToScoreString();
                     }
                     else
                     {
-                        scoreLabel.Text = (Players[0].Points).ToScoreString();
+                        lbl.Text = null;
                     }
+                }
+                i++;
+            }
+        }
 
-                    for (int i = 0; i < ScoreLabels.Length - 1; i++)
+        public virtual void UpdateMainScore()
+        {
+            if (pinGod?.Players?.Count <= 0) return;
+
+            if (scoreLabel != null)
+            {
+                if (pinGod.Player.Points > -1)
+                {
+                    //more than 1 player, show main score?
+                    if (pinGod.Players.Count > 1 && !_show_main_score_multiplayer)
                     {
-                        ScoreLabels[i].Text = (Players[i].Points).ToScoreString();
+                        scoreLabel.Text = null;
                     }
-
-                    if (ballInfolabel != null)
-                        ballInfolabel.Text = Tr("BALL") + " 3";
-
-                    if (playerInfoLabel != null)
-                        playerInfoLabel.Text = Tr("PLAYER") + " PLAYER: 1";
+                    //set main score label
+                    else { scoreLabel.Text = pinGod.Player.Points.ToScoreString(); }
+                }
+                else
+                {
+                    scoreLabel.Text = null;
                 }
             }
         }
 
         /// <summary>
-        /// override this but this will invoke <see cref="OnScoresUpdated"/> to update scene labels
+        /// Updates the current player and ball from PinGod BallInPlay and CurrentPlayerIndex
         /// </summary>
-        public virtual void OnBallStarted() => OnScoresUpdated();
+        public virtual void UpdatePlayerBallInfo()
+        {
+            if (pinGod?.Players?.Count <= 0) return;
+
+            if (ballInfolabel != null) 
+                ballInfolabel.Text = Tr("BALL") + " " + pinGod.BallInPlay.ToString();
+            if (playerInfoLabel != null) 
+                playerInfoLabel.Text = $"{Tr("PLAYER")}: {pinGod.CurrentPlayerIndex + 1}";
+        }
+
+        private void SetupDebugMode()
+        {
+            Players = new PinGodPlayer[] { new(), new(), new(), new() };
+            Logger.Debug(nameof(ScoreMode), $": DEBUG. Fake players");
+            Players[0].Points = 280000;
+            Players[1].Points = 260000;
+            Players[2].Points = 240000;
+            Players[3].Points = 220000;
+        }
     }
 }
