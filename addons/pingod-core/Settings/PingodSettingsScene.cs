6c7c9f3909b15d1934736cfda02c23c8da04b50d
Dave <horsepin@protonmail.com>
2023-01-25 14:27:52 +0000

    change settings scene to use the adjustments node and not PinGodGame

--- addons/pingod-core/Settings/PingodSettingsScene.cs
+++ addons/pingod-core/Settings/PingodSettingsScene.cs
@@ -1,14 +1,14 @@
 using Godot;
 using PinGod.Base;
 using PinGod.Core;
-using System;
+using PinGod.Core.Service;
 
 /// <summary>
 /// PinGod menu settings. Like log levels and machine read write
 /// </summary>
 public partial class PingodSettingsScene : MarginContainer
 {
-    private IPinGodGame pinGod;
+    private Adjustments _adjustments;
 
     /// <summary>
     /// Sets text of labels using language translation files <see cref="Godot.Object.Tr(string)"/>
@@ -16,9 +16,10 @@ public partial class PingodSettingsScene : MarginContainer
     public override void _EnterTree()
     {
         base._EnterTree();
-        if (HasNode("/root/PinGodGame"))
+        if (HasNode("/root/Adjustments"))
         {
-            pinGod = GetNode<IPinGodGame>("/root/PinGodGame");
+            var adjustments = GetNode<AdjustmentsNode>("/root/Adjustments");
+            _adjustments = adjustments._adjustments;
         }
     }
 
@@ -26,47 +27,47 @@ public partial class PingodSettingsScene : MarginContainer
     {
         base._Ready();
         var _stateDelaySpinbox = GetNode<SpinBox>("VBoxContainer/StatesDelaySpinBox");
-        _stateDelaySpinbox.Value = pinGod?.Adjustments?.MachineStatesWriteDelay ?? 10;
+        _stateDelaySpinbox.Value = _adjustments?.MachineStatesWriteDelay ?? 10;
         _stateDelaySpinbox.Prefix = Tr("SETT_STATE_DELAY");
 
         var _readStatesCheck = GetNode<CheckButton>("VBoxContainer/ReadStatesCheckButton");
-        _readStatesCheck.SetPressedNoSignal(pinGod?.Adjustments?.MachineStatesRead ?? true);
+        _readStatesCheck.SetPressedNoSignal(_adjustments?.MachineStatesRead ?? true);
         _readStatesCheck.Text = Tr("SETT_STATE_READ");
 
         var _writeStatsCheck = GetNode<CheckButton>("VBoxContainer/WriteStatesCheckButton");
-        _writeStatsCheck.SetPressedNoSignal(pinGod?.Adjustments?.MachineStatesWrite ?? true);
+        _writeStatsCheck.SetPressedNoSignal(_adjustments?.MachineStatesWrite ?? true);
         _writeStatsCheck.Text = Tr("SETT_STATE_WRITE");
 
         var logLvlSlider = GetNode<HSlider>("VBoxContainer/HBoxContainer/HSlider");
-        var lvl = pinGod?.Adjustments?.LogLevel ?? 0;
+        var lvl = _adjustments?.LogLevel ?? 0;
         logLvlSlider.Value = (int)lvl;
         UpdateLoggerText();
     }
 
     private void UpdateLoggerText()
     {
-        GetNode<Label>("VBoxContainer/HBoxContainer/Label2").Text = pinGod?.Adjustments?.LogLevel.ToString() ?? string.Empty;
+        GetNode<Label>("VBoxContainer/HBoxContainer/Label2").Text = _adjustments?.LogLevel.ToString() ?? string.Empty;
     }
 
     void _on_StatesDelaySpinBox_changed(int val)
     {
-        if (pinGod != null) pinGod.Adjustments.MachineStatesWriteDelay = val;
+        if (_adjustments != null) _adjustments.MachineStatesWriteDelay = val;
     }
 
     void _on_ReadStatesCheckButton_toggled(bool pressed)
     {
-        if (pinGod != null) pinGod.Adjustments.MachineStatesRead = pressed;
+        if (_adjustments != null) _adjustments.MachineStatesRead = pressed;
     }
 
     void _on_WriteStatesCheckButton_toggled(bool pressed)
     {
-        if (pinGod != null) pinGod.Adjustments.MachineStatesWrite = pressed;
+        if (_adjustments != null) _adjustments.MachineStatesWrite = pressed;
     }
 
     void _on_HSlider_value_changed(float val)
     {
         var lvl = (LogLevel)val;
-        if (pinGod != null) pinGod.Adjustments.LogLevel = lvl;
+        if (_adjustments != null) _adjustments.LogLevel = lvl;
         UpdateLoggerText();
     }
 }
