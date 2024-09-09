393fd35570ba10c003a108d4f2410b7aae0539e4
dave <horsepin@protonmail.com>
2024-09-09 18:59:16 +0100

    added: `PinGodGameControl` and `PinGodGameModeControl` which both derive from a Godot Control. These are common and have access to the IPinGodGame and Resources.

---
+++ addons/pingod-game/Control/PinGodGameControl.cs
@@ -0,0 +1,18 @@
+﻿using Godot;
+
+/// <summary>Uses Godot Node as a base to provide PinGodGame access to a class</summary>
+public abstract partial class PinGodGameControl : Control
+{
+    /// <summary>A reference to PinGodGame node</summary>
+    public IPinGodGame _pinGod;
+
+    /// <summary>A reference to PinGodGame resources</summary>
+    public Resources _resources;
+
+    /// <summary>Gets a reference to <see cref="pinGod"/> in the root /root/PinGodGame</summary>
+    public override void _EnterTree()
+    {
+        _pinGod = GetNodeOrNull(Paths.ROOT_PINGODGAME) as IPinGodGame;
+        _resources = GetNodeOrNull(Paths.ROOT_RESOURCES) as Resources;
+    }
+}
---
+++ addons/pingod-game/Control/PinGodGameModeControl.cs
@@ -0,0 +1,16 @@
+﻿public abstract partial class PinGodGameModeControl : PinGodGameControl
+{
+    /// <summary> Mode group signals</summary>
+    protected virtual void OnBallDrained() { }
+
+    /// <summary>Mode group signals</summary>
+    protected virtual void OnBallSaved() { }
+
+    /// <summary>Mode group signals</summary>
+    protected virtual void OnBallStarted() { }
+
+    /// <summary>Mode group signals</summary>
+    protected virtual void UpdateLamps() { }
+
+    public override void _EnterTree() => base._EnterTree();
+}
