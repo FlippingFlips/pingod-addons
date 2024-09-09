fd6c68a2e3abe08f337f4e09d4762b86d46c8648
dave <horsepin@protonmail.com>
2024-09-10 00:36:54 +0100

    - added a background layer for the multi-ball display, to cover the score. - multiballs shows the current players score and updates it when changed

--- addons/pingod-modes/multiball/Multiball.cs
+++ addons/pingod-modes/multiball/Multiball.cs
@@ -1,17 +1,15 @@
 using Godot;
+using PinGod.Base;
 using PinGod.Core.addons.ModeTimer;
 
 namespace PinGod.Modes
 {
-    /// <summary>
-    /// Starts multi-ball using pinGod and removes this scene when EndMultiball is called. <para/>
+    /// <summary>Starts multi-ball using pinGod and removes this scene when EndMultiball is called. <para/>
     /// Scenes that are in a group named multiball, like this scene file default is, will have EndMultiball invoked <see cref="EndMultiball"/> <para/>
     /// <see cref="Game.EndMultiball"/>
     /// </summary>
-    public partial class Multiball : Control
+    public partial class Multiball : PinGodGameControl
     {
-        private IPinGodGame pinGod;
-
         /// <summary>
         /// 4 ball multi-ball
         /// </summary>
@@ -20,18 +18,30 @@ namespace PinGod.Modes
         [Export] byte _ball_save_time_seconds = 12;
 
         [Export] bool _showModeTimer = true;
+        private Label _scoreLabel;
 
-        /// <summary>
-        /// Gets a reference for <see cref="pinGod"/>
-        /// </summary>
         public override void _EnterTree()
         {
-            pinGod = GetNode(Paths.ROOT_PINGODGAME) as IPinGodGame;
-            if (pinGod == null)
+            base._EnterTree();
+
+            if (_pinGod == null)
             {
                 Logger.Info(nameof(Multiball), " this module requires " + nameof(IPinGodGame), " in the root, exiting.");
                 this.QueueFree();
             }
+
+            _scoreLabel = GetNodeOrNull<Label>("ScoreLabel");
+            if(_scoreLabel != null)
+            {
+                //update the score label with current player score
+                _scoreLabel.Text = _pinGod.Player.Points
+                    .ToScoreString();
+
+                //anytime scores are updated we can update the score with the players score
+                _pinGod.Connect(
+                    nameof(PinGodBase.ScoresUpdated),
+                    Callable.From(() => _scoreLabel.Text = _pinGod.Player.Points.ToScoreString()));
+            }                
         }
 
         /// <summary>
@@ -49,7 +59,7 @@ namespace PinGod.Modes
                 timer.IsVisible(false);
 
             //run the multi-ball, kick balls every 2 secs
-            pinGod.StartMultiBall(_num_of_balls, _ball_save_time_seconds, 2);
+            _pinGod?.StartMultiBall(_num_of_balls, _ball_save_time_seconds, 2);
 
             //connect to the mode timer for when it times out
             //we can end the multiball
@@ -63,7 +73,7 @@ namespace PinGod.Modes
         public virtual void EndMultiball(string g)
         {
             Logger.Debug(nameof(Multiball), ":", nameof(EndMultiball));
-            pinGod.IsMultiballRunning = false;
+            _pinGod.IsMultiballRunning = false;
             this.QueueFree();
         }
     }
